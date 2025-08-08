48126ee44a2d5f357fd2b2a749079cd050455a5f
Konstantin <89670253660@mail.ru>
2025-08-08 13:54:44 +0300

    fix(webhook): добавил ответ вебхука по шаблону яндекса

--- alice/src/api/v1/alice.py
+++ alice/src/api/v1/alice.py
@@ -7,7 +7,18 @@ router = APIRouter()
 logger = logging.getLogger(__name__)
 
 
-@router.post("/webhook", summary="Alice webhook", response_model=None)
+@router.post("/webhook", summary="Alice webhook", response_model=dict)
 async def webhook(request: Request):
     logger.info(request)
     logger.info(request.json())
+    return {
+        "response": {
+            "text": "Здравствуйте! Это мы, хороводоведы.",
+            "tts": "Здравствуйте! Это мы, хороводоведы.",
+            "end_session": True
+        },
+        "application_state": {
+          "value": 37
+        },
+        "version": "1.0"
+    }
