1330838931ad3fc2bde179d6735d2c60f84e499d
Konstantin <89670253660@mail.ru>
2025-08-10 22:27:18 +0300

    fix(alice): учёл требования модератора

--- alice/src/api/v1/alice.py
+++ alice/src/api/v1/alice.py
@@ -10,7 +10,20 @@ logger = logging.getLogger(__name__)
 @router.post("/webhook", summary="Alice webhook", response_model=dict)
 async def webhook(request: Request):
     logger.info(request)
-    logger.info(request.json())
+    logger.info(await request.json())
+    request = await request.json()
+    if request["request"]["type"] == "Что ты умеешь":
+        return {
+            "response": {
+                "text": "Я умею искать режиссёра фильма. Спроси кто режиссёр фильма Star wars",
+                "tts": "Я умею искать режиссёра фильма. Спроси кто режиссёр фильма Star wars",
+                "end_session": False
+            },
+            "application_state": {
+                "value": 37
+            },
+            "version": "1.0"
+        }
     return {
         "response": {
             "text": "Здравствуйте! Это мы, хороводоведы.",
